d3e3f64153ea2f70fb99d7d85cea871db1c44b56
Jahnathan Davis <jrd58@boole.cs.calvin.edu>
2016-11-26 00:06:04 -0500

    Finished lab12

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, Redirect, browserHistory } from 'react-router';
 
 import CommentBox from './commentBox';
 import CommentEdit from './commentEdit';
 
 import '../css/base.css';
 
+import { Router, Route, Redirect, browserHistory } from 'react-router';
+
 ReactDOM.render((
     <Router history={browserHistory}>
         <Route path="/" component={CommentBox}/>
-        <Route path="/:id" component={CommentEdit}/>
+        <Route path="/:id" component={CommentEdit} />
     </Router>
-), document.getElementById('content')
+ 	),
+    document.getElementById('content')
 );
